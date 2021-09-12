2f2691acbe0990399f62e94ca08ab16855ff0e6c
Ariel Azzi <arielsazzi@gmail.com>
2021-09-12 15:11:37 -0300

    Adiciona codificação e decodificação Unária

---
+++ codification/unaria.py
@@ -0,0 +1,31 @@
+def encode(file):
+    file_content = file.read()
+    stop_bit = 1
+    encoded_text = ""
+    for letter in range(0, len(file_content)):
+        ascii_char = ord(file_content[letter])
+        encoded_text += bin(0)[2:].zfill(ascii_char) + str(stop_bit)
+
+    new_file = open(file.name + '.cod', 'w+')
+    new_file.writelines(str(30) + encoded_text[:-1])
+    print("Codificaçâo salva no arquivo: " + new_file.name)
+    new_file.close()
+
+
+def decode(file):
+    file_content = file.read()
+    decoded_text = ""
+    total = 0
+    for letter in range(0, len(file_content)):
+        if file_content[letter] == '0':
+            total += 1
+        else:
+            decoded_text = decoded_text + chr(total)
+            total = 0
+    decoded_text = decoded_text + chr(total)
+    print(decoded_text)
+
+    new_file = open(file.name[:len(file.name) - 8] + '_decoded.txt', 'w+')
+    new_file.writelines(decoded_text)
+    print("Decodificaçâo salva no arquivo: " + new_file.name)
+    new_file.close()
