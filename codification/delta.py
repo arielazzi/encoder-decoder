084bc7aefce16fd4e1144e768e128f1aab9e135d
RayanaRM <51447380+RayanaRM@users.noreply.github.com>
2021-10-03 15:49:38 -0300

    Update delta.py

--- codification/delta.py
+++ codification/delta.py
@@ -3,17 +3,19 @@ import utils
 def encode(file):
     file_content = file.read()
 
-    codification_type = '00000101'
-    golomb_divider = '00000000'
-
     encoded_text = ''
+    current_character = ''
 
     for i in range(0, len(file_content)):
-        print(file_content[i])
-        print(bin(file_content[i]))
-        encoded_text += bin(file_content[i])
+        character = format(file_content[i], "b") if current_character != file_content[i] else format(0, "b")
+        print(character)
+        current_character = file_content[i]
+        encoded_text += character
 
+    codification_type = "00000101"
+    golomb_divider = "00000000"  # Used only in Golomb encoding
     utils.write_file_in_bytes(codification_type + golomb_divider + encoded_text, file.name)
+    print("Codificação salva no arquivo: " + file.name + '.cod')
 
 def decode(file):
-    print('Decode Delta')
+    print("Decodificação Delta")
