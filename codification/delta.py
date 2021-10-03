344a71c97f58869ae5ec40ec9facd8e1e1c06679
Mateus <mateusdweber@gmail.com>
2021-10-03 18:35:25 -0300

    feat/delta: get text as bins of 8 bits

--- codification/delta.py
+++ codification/delta.py
@@ -3,8 +3,8 @@ import utils
 def encode(file):
     file_content = file.read()
 
-    encoded_text = ''
-    current_character = ''
+    encoded_text = ""
+    current_character = ""
 
     for i in range(0, len(file_content)):
         if current_character != file_content[i] and i == 0:
@@ -19,8 +19,20 @@ def encode(file):
     codification_type = "00000100"
     golomb_divider = "00000000"  # Used only in Golomb encoding
     utils.write_file_in_bytes(codification_type + golomb_divider + encoded_text, file.name)
-    print("Codificação salva no arquivo: " + file.name + '.cod')
+    print("Codificação salva no arquivo: " + file.name + ".cod")
 
 def decode(file):
     file_content = utils.binary_file_to_string(file)
-    print(file_content)
+
+    decoded_text = ""
+    current_character_to_decode = ""
+    counter = 0
+
+    for i in range(0, len(file_content)):
+        if counter < 8:
+            current_character_to_decode += file_content[i]
+            counter += 1
+        else:
+            print(current_character_to_decode)
+            current_character_to_decode = ""
+            counter = 0
