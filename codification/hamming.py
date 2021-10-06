93175a281b2ad30f2826f8cdc301caface78603c
Ariel Azzi <arielsazzi@gmail.com>
2021-10-05 21:29:08 -0300

    Retorna resultado do hamming

--- codification/hamming.py
+++ codification/hamming.py
@@ -35,6 +35,7 @@ def decode(file):
     file_content = utils.binary_file_to_string(file)
     i = 0
     print(file_content)
+    output = ''
     while i < len(file_content):
         print(file_content[i:i + 8])
         print('----')
@@ -45,37 +46,80 @@ def decode(file):
         if not op5 and op6 and op7:  # Error only on top
             print("O primeiro bit de paridade está com erro")
             log.writelines('[' + str(datetime.now()) + ']' + ' O primeiro bit de paridade está errado, ajustado bit de paridade\n')
-            print(file_content[i], file_content[i + 1], file_content[i + 2], file_content[i + 3], '1' if file_content[i + 4] == '0' else '0', file_content[i + 5], file_content[i + 6]) # o novo resultados
+            print(file_content[i], file_content[i + 1], file_content[i + 2], file_content[i + 3], '1' if file_content[i + 4] == '0' else '0', file_content[i + 5], file_content[i + 6])  # o novo resultados
+            output += file_content[i] + file_content[i + 1] + file_content[i + 2] + file_content[i + 3] + ('1' if file_content[i + 4] == '0' else '0') + file_content[i + 5] + file_content[i + 6] + '0'
             print('----')
+        else:
+            output += file_content[i:i + 5]
+            i += 8
+            continue
+
         if not op5 and not op6 and op7:  # Error on top and right
             print("O primeiro e o segundo bit de paridade estão com erro")
             log.writelines('[' + str(datetime.now()) + ']' + ' O primeiro e o segundo bit de paridade estão com erro\n')
             print(file_content[i], '1' if file_content[i + 1] == '0' else '0', file_content[i + 2], file_content[i + 3], file_content[i + 4], file_content[i + 5], file_content[i + 6])
+            output += file_content[i] + ('1' if file_content[i + 1] == '0' else '0') + file_content[i + 2] + file_content[i + 3] + file_content[i + 4] + file_content[i + 5] + file_content[i + 6] + '0'
             print('----')
+        else:
+            output += file_content[i:i + 5]
+            i += 8
+            continue
+
         if not op5 and not op7 and op6:  # Error on top and left
             print("O primeiro e o terceiro bit de paridade estão com erro")
             log.writelines('[' + str(datetime.now()) + ']' + ' O primeiro e o terceiro bit de paridade estão com erro\n')
             print('1' if file_content[i] == '0' else '0', file_content[i + 1], file_content[i + 2], file_content[i + 3], file_content[i + 4], file_content[i + 5], file_content[i + 6])
+            output += ('1' if file_content[i] == '0' else '0') + file_content[i + 1] + file_content[i + 2] + file_content[i + 3] + file_content[i + 4] + file_content[i + 5] + file_content[i + 6] + '0'
             print('----')
+        else:
+            output += file_content[i:i + 5]
+            i += 8
+            continue
+
         if not op5 and not op6 and not op7:  # Error on top, left and right
             print("O primeiro, o segundo e o terceiro bit de paridade estão com erro")
             log.writelines('[' + str(datetime.now()) + ']' + ' O primeiro, o segundo e o terceiro bit de paridade estão com erro\n')
             print(file_content[i], file_content[i + 1], '1' if file_content[i + 2] == '0' else '0', file_content[i + 4], file_content[i + 4], file_content[i + 5], file_content[i + 6])
+            output += file_content[i] + file_content[i + 1] + ('1' if file_content[i + 2] == '0' else '0') + file_content[i + 4] # + file_content[i + 4] + file_content[i + 5] + file_content[i + 6] + '0'
             print('----')
+        else:
+            output += file_content[i:i + 5]
+            i += 8
+            continue
+
         if not op6 and op5 and op7:  # Error on left
             print("O segundo bit de paridade está com erro")
             log.writelines('[' + str(datetime.now()) + ']' + ' O segundo bit de paridade está com erro\n')
             print(file_content[i], file_content[i + 1], file_content[i + 2], file_content[i + 3], file_content[i + 4], '1' if file_content[i + 5] == '0' else '0', file_content[i + 6])
+            output += file_content[i] + file_content[i + 1] + file_content[i + 2] + file_content[i + 3] # + file_content[i + 4] + ('1' if file_content[i + 5] == '0' else '0') + file_content[i + 6] + '0'
             print('----')
+        else:
+            output += file_content[i:i + 5]
+            i += 8
+            continue
+
         if not op6 and not op7 and op5:  # Error on left and right
             print("O segundo e o terceiro bit de paridade estão com erro")
             log.writelines('[' + str(datetime.now()) + ']' + ' O segundo e o terceiro bit de paridade estão com erro\n')
             print(file_content[i], file_content[i + 1], file_content[i + 2], '1' if file_content[i + 3] == '0' else '0', file_content[i + 4], file_content[i + 5], file_content[i + 6])
+            output += file_content[i] + file_content[i + 1] + file_content[i + 2] + ('1' if file_content[i + 3] == '0' else '0') #+ file_content[i + 4] + file_content[i + 5] + file_content[i + 6] + '0'
             print('----')
+        else:
+            output += file_content[i:i + 5]
+            i += 8
+            continue
+
         if not op7 and op5 and op6:  # Error on right
             print("O terceiro bit de paridade está com erro")
             log.writelines('[' + str(datetime.now()) + ']' + ' O terceiro bit de paridade está errado\n')
             print(file_content[i], file_content[i + 1], file_content[i + 2], file_content[i + 3], file_content[i + 4], file_content[i + 5], '1' if file_content[i + 6] == '0' else '0')
+            output += file_content[i] + file_content[i + 1] + file_content[i + 2] + file_content[i + 3] #+ file_content[i + 4] + file_content[i + 5] + ('1' if file_content[i + 6] == '0' else '0') + '0'
             print('----')
+        else:
+            output += file_content[i:i + 5]
+            i += 8
+            continue
 
         i += 8
+    print(output)
+    return output
